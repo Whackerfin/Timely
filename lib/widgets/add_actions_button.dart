4eb4754fc03a9c3358b6b159b758b569be19f972
Whackerfin <jake04mathew@gmail.com>
2025-01-13 19:59:57 +0530

    Fixed Add button animation for final

--- lib/widgets/add_actions_button.dart
+++ lib/widgets/add_actions_button.dart
@@ -7,11 +7,29 @@ class AddActionsButton extends StatefulWidget {
   State<AddActionsButton> createState() => _AddActionsState();
 }
 
-class _AddActionsState extends State<AddActionsButton> {
+class _AddActionsState extends State<AddActionsButton> with SingleTickerProviderStateMixin {
   bool _isPressed = false;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 400),
+      vsync: this
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   void press() {
     setState(() {
       _isPressed = !_isPressed;
+      _isPressed ? _controller.forward() : _controller.reverse();
     });
   }
 
@@ -19,62 +37,69 @@ class _AddActionsState extends State<AddActionsButton> {
   Widget build(BuildContext context) {
     return AnimatedContainer(
         duration: const Duration(milliseconds: 300),
-        padding: EdgeInsets.only(top: 20.0, bottom: 25.0),
-        child: AnimatedSwitcher(
-            duration: Duration(milliseconds: 300),
-            transitionBuilder: (child, animation) {
-              return SizeTransition(
-                sizeFactor: animation, // Animates the size factor
-                axis: Axis.vertical, // Expand vertically
-                child: child,
+        padding: const EdgeInsets.symmetric(vertical: 20.0),
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          SizedBox(
+              height: 65.0,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12.0),
+                        topRight: Radius.circular(12.0),
+                        bottomLeft:
+                            _isPressed ? Radius.zero : Radius.circular(12.0),
+                        bottomRight:
+                            _isPressed ? Radius.zero : Radius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                  backgroundColor: WidgetStateProperty.resolveWith<Color?>(
+                    (Set<WidgetState> states) {
+                      if (states.contains(WidgetState.pressed)) {
+                        return Theme.of(context)
+                            .colorScheme
+                            .secondary
+                            .withAlpha(180);
+                      }
+                      return Theme.of(context).colorScheme.secondary;
+                    },
+                  ),
+                ),
+                onPressed: press,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "ADD AN ACTION ",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    AnimatedRotation(
+                      turns: _isPressed ? 0.5 : 0.0,
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                      child: Icon(
+                        Icons.keyboard_arrow_up,
+                        color: Theme.of(context).colorScheme.onSecondary,
+                        size: 32.0,
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return ClipRect(
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  heightFactor: _controller.value,
+                  child: ActionsForm()
+                ),
               );
             },
-            child: !_isPressed
-                ? Row(children: <Widget>[
-                    Expanded(
-                      child: Container(
-                          constraints: BoxConstraints(minHeight: 65.0),
-                          child: ElevatedButton(
-                              style: ButtonStyle(
-                                shape: WidgetStateProperty.all<
-                                        RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(12.0))),
-                                backgroundColor:
-                                    WidgetStateProperty.resolveWith<Color?>(
-                                  (Set<WidgetState> states) {
-                                    if (states.contains(WidgetState.pressed)) {
-                                      return Theme.of(context)
-                                          .colorScheme
-                                          .secondary
-                                          .withAlpha(180);
-                                    }
-                                    return Theme.of(context)
-                                        .colorScheme
-                                        .secondary; // Use the component's default.
-                                  },
-                                ),
-                              ),
-                              onPressed: () {
-                                press();
-                              },
-                              child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text("ADD AN ACTION ",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .titleMedium),
-                                    Icon(Icons.keyboard_arrow_up,
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .onSecondary,
-                                        size: 32.0),
-                                  ]))),
-                    )
-                  ])
-                : ActionsForm(onPressed: press)));
+          ),
+        ]));
   }
 }
--- lib/widgets/add_form.dart
+++ lib/widgets/add_form.dart
@@ -1,50 +1,19 @@
 import "package:flutter/material.dart";
 
 class ActionsForm extends StatelessWidget {
-  final VoidCallback onPressed;
-  const ActionsForm({required this.onPressed, super.key});
+  const ActionsForm({super.key});
   @override
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.only(bottom: 26.0),
         decoration: BoxDecoration(
             color: Theme.of(context).colorScheme.secondary,
-            borderRadius: BorderRadius.circular(12.0)),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.zero,
+                topRight: Radius.zero,
+                bottomRight: Radius.circular(12.0),
+                bottomLeft: Radius.circular(12.0))),
         child: Column(children: [
-          SizedBox(
-              width: double.infinity,
-              height: 65.0,
-              child: TextButton.icon(
-                style: ButtonStyle(
-                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(12.0),
-                              topRight: Radius.circular(12.0),
-                              bottomLeft: Radius.zero,
-                              bottomRight: Radius.zero))),
-                  backgroundColor: WidgetStateProperty.resolveWith<Color?>(
-                    (Set<WidgetState> states) {
-                      if (states.contains(WidgetState.pressed)) {
-                        return Theme.of(context)
-                            .colorScheme
-                            .secondary
-                            .withAlpha(180);
-                      }
-                      return Theme.of(context)
-                          .colorScheme
-                          .secondary; // Use the component's default.
-                    },
-                  ),
-                ),
-                onPressed: onPressed,
-                iconAlignment: IconAlignment.end,
-                icon: Icon(Icons.keyboard_arrow_down,
-                    color: Theme.of(context).colorScheme.onSecondary,
-                    size: 32.0),
-                label: Text("ADD AN ACTION ",
-                    style: Theme.of(context).textTheme.titleMedium),
-              )),
           Padding(
               padding: EdgeInsets.symmetric(horizontal: 26.0),
               child: Row(children: [
