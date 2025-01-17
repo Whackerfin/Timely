da9b2f1e872c26a79770ae394b9a655ce2312853
Whackerfin <jake04mathew@gmail.com>
2025-01-17 16:09:02 +0530

    Duration picker made

--- lib/widgets/add_form.dart
+++ lib/widgets/add_form.dart
@@ -1,6 +1,8 @@
+import "dart:ui";
+
 import "package:flutter/material.dart";
-import 'package:day_night_time_picker/day_night_time_picker.dart';
 import "package:provider/provider.dart";
+import 'package:wheel_picker/wheel_picker.dart';
 import "package:timely/models/actionmodel.dart";
 import "package:timely/providers/actions_provider.dart";
 
@@ -18,23 +20,174 @@ class _ActionFormState extends State<ActionForm> {
   String name = "";
   String duration = "";
   String icon = "";
-  Time _time = Time(hour: 0, minute: 30);
-  void _showTimePicker() {
-    Navigator.of(context).push(showPicker(
-        value: _time,
-        blurredBackground: true,
-        onChange: onTimeChange,
-        context: context,
-        backgroundColor: Theme.of(context).colorScheme.secondary,
-        is24HrFormat: false,
-        accentColor: Theme.of(context).colorScheme.onSecondary));
+  int _hours = 0;
+  int _mins = 0;
+  late final _hoursWheel = WheelPickerController(
+    itemCount: 13,
+    initialIndex: _hours % 12,
+  );
+  late final _minsWheel = WheelPickerController(
+      itemCount: 60, initialIndex: _mins, mounts: [_hoursWheel]);
+  void setTime(hours, mins) {
+    String time = "";
+    if (hours != 0) {
+      time = "$hours" "hrs ";
+    }
+    if (mins != 0) {
+      time += "$mins" "mins";
+    }
+    if (hours == 0 && mins == 0) {
+      time = "--";
+    }
+    setState(() {
+      duration = time;
+      timefieldText.text = time;
+    });
   }
 
-  void onTimeChange(Time time) {
+  void acceptTime() {
     setState(() {
-      _time = time;
-      timefieldText.text = _time.format(context);
+      _hours = _hoursWheel.selected;
+      _mins = _minsWheel.selected;
+      Navigator.of(context).pop();
+      setTime(_hours, _mins);
     });
+  }
+
+  void showOverlay(BuildContext context, timeWheels) {
+    showDialog(
+      context: context,
+      barrierColor: Colors.black.withAlpha(127), // Semi-transparent background
+      barrierDismissible: true, // Dismiss on tapping outside
+      builder: (context) {
+        return BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5), // Blur effect
+          child: Center(
+            child: Container(
+              width: 400.0,
+              height: 500.0,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(12)),
+              child: Stack(
+                children: [
+                  Container(
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+                    child: Text(
+                      "Pick how long it will take you to complete your action",
+                      style: Theme.of(context).textTheme.headlineSmall,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Positioned(
+                      right: 90,
+                      top: 0,
+                      bottom: 0,
+                      child: Padding(
+                          padding: EdgeInsets.only(top: 40.0),
+                          child: centerBar(context))),
+                  Padding(
+                      padding:
+                          EdgeInsets.only(left: 100.0, right: 100.0, top: 40.0),
+                      child: Container(
+                        width: 200.0,
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ...timeWheels,
+                            const SizedBox(width: 6.0),
+                          ],
+                        ),
+                      )),
+                  Positioned(
+                      bottom: 10,
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: WidgetStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(12.0),
+                                        topRight: Radius.circular(12.0),
+                                        bottomLeft: Radius.circular(12.0),
+                                        bottomRight: Radius.circular(12.0),
+                                      ),
+                                    ),
+                                  ),
+                                  backgroundColor:
+                                      WidgetStateProperty.resolveWith<Color?>(
+                                    (Set<WidgetState> states) {
+                                      if (states
+                                          .contains(WidgetState.pressed)) {
+                                        return Theme.of(context)
+                                            .colorScheme
+                                            .error
+                                            .withAlpha(180);
+                                      }
+                                      return Theme.of(context)
+                                          .colorScheme
+                                          .error;
+                                    },
+                                  ),
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  "Cancel",
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                )),
+                            SizedBox(width: 30.0),
+                            ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: WidgetStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(12.0),
+                                        topRight: Radius.circular(12.0),
+                                        bottomLeft: Radius.circular(12.0),
+                                        bottomRight: Radius.circular(12.0),
+                                      ),
+                                    ),
+                                  ),
+                                  backgroundColor:
+                                      WidgetStateProperty.resolveWith<Color?>(
+                                    (Set<WidgetState> states) {
+                                      if (states
+                                          .contains(WidgetState.pressed)) {
+                                        return Theme.of(context)
+                                            .colorScheme
+                                            .secondary
+                                            .withAlpha(180);
+                                      }
+                                      return Theme.of(context)
+                                          .colorScheme
+                                          .secondary;
+                                    },
+                                  ),
+                                ),
+                                onPressed: () {
+                                  acceptTime();
+                                },
+                                child: Text("Ok",
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall))
+                          ]))
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   void clearText() {
@@ -85,7 +238,69 @@ class _ActionFormState extends State<ActionForm> {
   }
 
   @override
+  void dispose() {
+    // Don't forget to dispose the controllers at the end.
+    _hoursWheel.dispose();
+    _minsWheel.dispose();
+    super.dispose();
+  }
+
+  Widget centerBar(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 220.0,
+        height: 38.0,
+        decoration: BoxDecoration(
+          color: const Color(0xFFC3C9FA).withAlpha(26),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    const textStyle = TextStyle(fontSize: 32.0, height: 1.5);
+    final wheelStyle = WheelPickerStyle(
+      itemExtent: textStyle.fontSize! * textStyle.height!, // Text height
+      squeeze: 1.25,
+      diameterRatio: .8,
+      surroundingOpacity: .25,
+      magnification: 1.2,
+    );
+    Widget itemBuilder(BuildContext context, int index) {
+      return Text("$index".padLeft(2, '0'), style: textStyle);
+    }
+
+    final timeWheels = <Widget>[
+      Expanded(
+        child: WheelPicker(
+          builder: itemBuilder,
+          controller: _hoursWheel,
+          looping: false,
+          style: wheelStyle,
+          selectedIndexColor: Colors.redAccent,
+        ),
+      ),
+      Align(
+        alignment: Alignment.center,
+        child: Text("hrs", style: textStyle),
+      ),
+      Expanded(
+        child: WheelPicker(
+          builder: itemBuilder,
+          controller: _minsWheel,
+          looping: true,
+          style: wheelStyle,
+          selectedIndexColor: Colors.redAccent,
+        ),
+      ),
+      Align(
+        alignment: Alignment.center,
+        child: Text("mins", style: textStyle),
+      ),
+    ];
+
     return Container(
         padding: EdgeInsets.only(bottom: 26.0),
         decoration: BoxDecoration(
@@ -161,12 +376,9 @@ class _ActionFormState extends State<ActionForm> {
                         SizedBox(width: 10.0),
                         Expanded(
                             child: TextField(
-                          onTap: _showTimePicker,
                           controller: timefieldText,
-                          onChanged: (text) {
-                            setState(() {
-                              duration = text;
-                            });
+                          onTap: () {
+                            showOverlay(context, timeWheels);
                           },
                           decoration: InputDecoration(
                             maintainHintHeight: true,
