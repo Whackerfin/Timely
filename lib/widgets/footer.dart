abd0b3d43a44d7444b698a03fce1fa4be4d92ddf
Whackerfin <jake04mathew@gmail.com>
2025-01-17 11:59:42 +0530

    footer set to state

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,17 +1,49 @@
+import "package:day_night_time_picker/day_night_time_picker.dart";
 import "package:flutter/material.dart";
 
-class Footer extends StatelessWidget {
+class Footer extends StatefulWidget {
   const Footer({super.key});
   @override
+  State<Footer> createState() => _FooterState();
+}
+
+class _FooterState extends State<Footer> {
+  Time _time = Time(hour: 8, minute: 30);
+  final timefieldText = TextEditingController();
+  void _showTimePicker() {
+    Navigator.of(context).push(showPicker(
+        value: _time,
+        blurredBackground: true,
+        onChange: onTimeChange,
+        context: context,
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        is24HrFormat: false,
+        accentColor: Theme.of(context).colorScheme.onSecondary));
+  }
+
+  void onTimeChange(Time time) {
+    setState(() {
+      _time = time;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
+    timefieldText.text = _time.format(context);
+    return Container(
         padding: EdgeInsets.only(bottom: 8.0),
         child: Row(
+          mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text("READY@", style: Theme.of(context).textTheme.bodyMedium),
             SizedBox(width: 32.0),
-            Text("8:00 AM", style: Theme.of(context).textTheme.displaySmall)
+            TextButton(
+                onPressed: () {
+                  _showTimePicker();
+                },
+                child: Text(_time.format(context),
+                    style: Theme.of(context).textTheme.displaySmall))
           ],
         ));
   }
